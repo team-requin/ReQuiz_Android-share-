abaae6a2501f650c31b6fba389e7ac0eb339604e
woochanlee <030219woo@naver.com>
2019-07-13 22:33:22 +0900

    Register Check
    Id Check
    Pw Check

--- app/src/main/java/com/example/requiz/Sign_up.java
+++ app/src/main/java/com/example/requiz/Sign_up.java
@@ -32,50 +32,92 @@ public class Sign_up extends AppCompatActivity {
         final Button ID_check_button2 = findViewById(R.id.ID_check_button2);
         JsonLoginApi jsonSignUpAp;
 
-
         Nickname_check_button.setOnClickListener(new View.OnClickListener() {
-                                                     @Override
-                                                     public void onClick(View v) {
-                                                         String name;
-                                                         name = Signup_Nickname.getText().toString();
-                                                         RetrofitConnection retrofitConnection = new RetrofitConnection();
-                                                         JsonObject jsonObject = new JsonObject();
-                                                         jsonObject.addProperty("name", name);
+            @Override
+            public void onClick(View v) {
+                String name;
+                name = Signup_Nickname.getText().toString();
+                RetrofitConnection retrofitConnection = new RetrofitConnection();
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("name", name);
 
-                                                         Call<Void> call = retrofitConnection.sever.checkSameName(jsonObject);
-                                                         call.enqueue(new Callback<Void>() {
-                                                             @Override
-                                                             public void onResponse(Call<Void> call, Response<Void> response) {
-                                                                 if (response.code() / 100 == 2) {
-                                                                     AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
-                                                                     builder.setMessage("사용가능한 닉네임입니다.")
-                                                                             .setPositiveButton("확인", null)
-                                                                             .show();
-                                                                 }
-                                                             }
+                Call<Void> call = retrofitConnection.sever.checkSameName(jsonObject);
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.code() / 100 == 2) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
+                            builder.setMessage("사용가능한 닉네임입니다.")
+                                    .setPositiveButton("확인", null)
+                                    .show();
+                        }
+                        else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
+                            builder.setMessage("이미 등록된 닉네임입니다.")
+                                    .setPositiveButton("실패", null)
+                                    .show();
+                        }
+                    }
 
-                                                             @Override
-                                                             public void onFailure(Call<Void> call, Throwable t) {
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
 
-                                                             }
-                                                         });
-                                                     }
-                                                 });
+                    }
+                });
+            }
+        });
 
+        ID_check_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getId = Signup_ID2.getText().toString();
+
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id",getId);
+
+                RetrofitConnection retrofitConnection = new RetrofitConnection();
+                Call<Void> call = retrofitConnection.sever.checkSameId(jsonObject);
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+
+                        if (response.code() / 100 == 2) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
+                            builder.setMessage("사용가능한 아이디입니다." + response.code())
+                                    .setNegativeButton("확인", null)
+                                    .show();
+                        }
+                        else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
+                            builder.setMessage("이미 등록된 아이디입니다" + response.code())
+                                    .setNegativeButton("다시시도", null)
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+
+                    }
+                });
+
+            }
+        });
 
         Signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String getUserNickname = Signup_Nickname.getText().toString();
                 String getId = Signup_ID2.getText().toString();
-                String getPw = Signup_Password.getText().toString();
-                String getPwChk = Signup_pw_confirm.getText().toString();
+                String getPassword = Signup_Password.getText().toString();
+                String getPasswordCheck = Signup_pw_confirm.getText().toString();
+                String getName = Signup_Nickname.getText().toString();
+
 
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("id",getId);
-                jsonObject.addProperty("pw",getPw);
-                jsonObject.addProperty("pw_check",getPwChk);
-                jsonObject.addProperty("name",getUserNickname);
+                jsonObject.addProperty("pw",getPassword);
+                jsonObject.addProperty("pw_check",getPasswordCheck);
+                jsonObject.addProperty("name",getName);
 
                 RetrofitConnection retrofitConnection = new RetrofitConnection();
                 Call<Void> call = retrofitConnection.sever.register(jsonObject);
@@ -85,16 +127,22 @@ public class Sign_up extends AppCompatActivity {
 
                         if (response.code() / 100 == 2) {
                             AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
-                            builder.setMessage("if error" + response.code())
-                                    .setNegativeButton("if error", null)
+                            builder.setMessage("회원가입 성공" + response.code())
+                                    .setNegativeButton("확인", null)
                                     .show();
                             Intent intent = new Intent(Sign_up.this, Login.class);
                             startActivity(intent);
                         }
+                        else if (response.code() == 406){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
+                            builder.setMessage("비밀번호가 일치하지 않습니다." + response.code())
+                                    .setNegativeButton("다시시도", null)
+                                    .show();
+                        }
                         else{
                             AlertDialog.Builder builder = new AlertDialog.Builder(Sign_up.this);
-                            builder.setMessage("else error" + response.code())
-                                    .setNegativeButton("else error", null)
+                            builder.setMessage("이미 등록된 아이디입니다" + response.code())
+                                    .setNegativeButton("다시시도", null)
                                     .show();
                         }
                     }
